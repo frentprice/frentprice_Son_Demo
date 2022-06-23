9a7e8958e539644a3336e32bf275a71181e12a3a
Nurseda Diker <54947744+nurseda-diker@users.noreply.github.com>
2022-06-23 16:51:01 +0300

    password sayfası güncellendi

--- frontend/src/pages/PersonalPages/PasswordPage/PasswordPage.js
+++ frontend/src/pages/PersonalPages/PasswordPage/PasswordPage.js
@@ -6,6 +6,8 @@ import { TextField } from '../../RegisterPage/TextField';
 import * as Yup from "yup";
 import { Formik, Form } from "formik";
 import { Link } from "react-router-dom";
+import { Dropdown } from 'react-bootstrap';
+import logo from "../../assets/logo.jpeg";
 
 
 const ColoredLine = ({ color }) => (
@@ -29,21 +31,72 @@ export default function PasswordPage({currentUser, handleLogout}) {
           .required("Zorunlu"),
       });
   return (
-    <Formik
+    <div>
+       <nav className="border-bottom border-secondary" style={{padding: '10px'}}>
+        <div style={{fontSize:"30px"}}>
+        <a href="/" style={{textDecoration:"none"}}>
+        <img style={{width:"70px",paddingLeft:"30px"}} src={logo}></img>
+           <span style={{paddingLeft:"10px"}}>FRENTPRICE</span>
+           </a>
+        </div>
+      
+      {currentUser ? (
+        
+        <div className="buttons">
+          <Dropdown>
+            <Dropdown.Toggle variant="primary" id="dropdown-basic">
+              {currentUser}
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item as={Link} to="/profile">Profil</Dropdown.Item>
+              <Dropdown.Item as={Link} to="/favori">Favorilerim</Dropdown.Item>
+              <Dropdown.Item as={Link} to="/password">Şifre Değiştir</Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Item as={Link} to="/" onClick={handleLogout}>
+                Log out
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+        
+      ) : (
+        
+        <div className="buttons">
+          <Dropdown
+          //   style={{ float: "right", }}
+          >
+            <Dropdown.Toggle variant="primary" id="dropdown-basic">
+              {currentUser}
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item href="/profile">Profil</Dropdown.Item>
+              <Dropdown.Item href="/favori">Favorilerim</Dropdown.Item>
+              <Dropdown.Item href="/password">Şifre Değiştir</Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Item href="/" onClick={handleLogout}>
+                Log out
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+          </div>
+      ) }
+       </nav>
+      <Formik
       initialValues={{
         newPassword: "",
         confirmNewPassword: "",
       }}
       validationSchema={validate}
     >
+     
       {(formik) => (
     <div class="container mt-5">
         <div class="row">
             <div class="col-md-4">
-            <div className="border border-light" style={{height:"130%",width:"90%",backgroundColor:"#e9e9e9",padding:"6%"}} >
+            <div className="border border-light" style={{height:"80%",width:"90%",backgroundColor:"#e9e9e9",padding:"6%"}} >
           <div>
           <FontAwesomeIcon icon={ faUserCircle} size={"5x"}/>
-          <span>Kullanıcı Adı Soyadı</span>
+          <span style={{position:"fixed",top:"170px",left:"240px"}}> {currentUser} </span>
           <div>
       <ColoredLine color="#000"/>
     </div>
@@ -57,7 +110,7 @@ export default function PasswordPage({currentUser, handleLogout}) {
           <Link to="/profile">
                 <Button
                   variant="outline-primary"
-                  style={{ width: "60%", borderColor: "black" }}
+                  style={{ width: "60%", borderColor: "black",marginLeft:"17%" }}
                 >
                   <span style={{ color: "black" }}>Profil</span>
                 </Button>
@@ -65,28 +118,28 @@ export default function PasswordPage({currentUser, handleLogout}) {
           </div>
           <div className="mt-5">
           <Link to="/favori">
-          <Button variant="outline-primary" style={{width:"60%",borderColor:"black"}}><span style={{color:"black"}}>Favorilerim</span></Button>
+          <Button variant="outline-primary" style={{width:"60%",borderColor:"black",marginLeft:"17%" }}><span style={{color:"black"}}>Favorilerim</span></Button>
               </Link>
           
           </div>
           <div className="mt-5">
-          <Button style={{width:"60%"}}><span style={{color:"black"}}>Şifre Değiştir</span></Button>
+          <Button style={{width:"60%",marginLeft:"17%"}}><span style={{color:"black"}}>Şifre Değiştir</span></Button>
           </div>
         </div>
             </div>
             <div class="col-md-8">
                 <h2 style={{textAlign:"center",fontWeight:"bold"}}>Şifre Değiştir</h2>
-                <div className="border border-dark mt-5 " style={{width:"60%",height:"90%",paddingLeft:"16%",paddingTop:"5%",marginLeft:"20%"}}>
+                <div className="border border-dark mt-1 " style={{width:"60%",height:"70%",paddingLeft:"16%",paddingTop:"5%",marginLeft:"20%"}}>
 
                 <Form>
-                <div style={{width:"60%",verticalAlign:"middle",float:"left"}}>
-                <label style={{widht:"auto",float:"left"}}>Mevcut Şifre</label>
-                <TextField name="password" type="password" disabled/>
+                <div  className='mt-3' style={{width:"70%",verticalAlign:"middle",float:"left"}}>
+                {/* <label style={{widht:"auto",float:"left"}}>Mevcut Şifre</label> */}
+                <TextField label="Mevcut Şifre" name="password" type="password" disabled/>
                 </div>
-                <div style={{width:"60%"}}>
+                <div className='mt-4' style={{width:"70%"}}>
                 <TextField label="Şifre" name="newPassword" type="password" placeholder="Şifre" />
                 </div>
-                <div style={{width:"60%"}}>
+                <div className='mt-4' style={{width:"70%"}}>
                   <TextField
                     label="Şifreyi Onaylayın"
                     name="confirmNewPassword"
@@ -97,8 +150,8 @@ export default function PasswordPage({currentUser, handleLogout}) {
                 </Form>  
               
             
-            <div style={{paddingLeft:"18%"}}>
-                  <button className="btn btn-primary mt-3  " type="submit">
+            <div style={{paddingLeft:"18%",position:"fixed",top:"610px",right:"500px"}}>
+                  <button className="btn btn-primary" type="submit">
                     Kaydet
                   </button></div>
                 </div>
@@ -107,5 +160,7 @@ export default function PasswordPage({currentUser, handleLogout}) {
     </div>
    )}
    </Formik>
+   </div>
+   
  );
 }
